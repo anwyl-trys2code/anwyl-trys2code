9511e78b563f8b967260d9d14c823c9fccb16088
Dinoliz <69924160+anwyl-trys2code@users.noreply.github.com>
2024-06-03 23:14:36 -0400

    Add files via upload

---
+++ Greeting_2.py
@@ -0,0 +1,21 @@
+#take in input from user
+
+#just follow the instructions and dont look at the rest of the code
+
+firstname = input("Enter your first name ")
+
+lastname = input("Enter your last name ")
+
+if firstname == "Steven":
+    print ("Welcome, ", firstname, lastname, "my love")
+elif firstname == "steven":
+    print ("Welcome, ", firstname, lastname, "my love")
+elif lastname == "betances":
+     print ("Welcome, ", firstname, lastname, "my love")
+elif lastname == "Betances":
+      print ("Welcome, ", firstname, lastname, "my love")
+else:
+     print ("Welcome,", firstname, lastname, "nice to meet you.")
+
+
+
